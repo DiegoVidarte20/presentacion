a80def7e85f1cda3adc5649c95b09162b4bdfb6c
DiegoVidarte20 <diegovidarte2022@gmail.com>
2026-01-15 10:29:17 -0500

    maximizar

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
-import 'slides/slides.dart';
+import 'package:window_manager/window_manager.dart';
 
+import 'slides/slides.dart';
 import 'package:video_player_media_kit/video_player_media_kit.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // video_player_media_kit
   VideoPlayerMediaKit.ensureInitialized(windows: true);
+
+  // window_manager
+  await windowManager.ensureInitialized();
+
+  const windowOptions = WindowOptions(
+    size: Size(1920, 1080),          // 👈 ventana grande (cámbialo a 1920x1080 si quieres)
+    minimumSize: Size(1280, 720),   // 👈 para que no lo achiquen demasiado
+    center: true,
+    title: 'Presentacion',
+  );
+
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.maximize();
+    await windowManager.focus();
+  });
+
   runApp(const App());
 }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,14 +7,18 @@ import Foundation
 
 import media_kit_video
 import package_info_plus
+import screen_retriever_macos
 import video_player_avfoundation
 import volume_controller
 import wakelock_plus
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MediaKitVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitVideoPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
   VolumeControllerPlugin.register(with: registry.registrar(forPlugin: "VolumeControllerPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
